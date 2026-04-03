40d37da5de708cc5aa5f4ee68aa37d271a5157b4
Jayraj-Khule <jayrajkhule18@gmail.com>
2026-04-04 01:59:27 +0530

    chore: upload journey photos and dist files

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -10,7 +10,10 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
 // Middleware
-app.use((0, cors_1.default)());
+// Enable CORS for all origins, to ensure it works smoothly on all devices and browsers!
+app.use((0, cors_1.default)({
+    origin: "*"
+}));
 app.use(express_1.default.json());
 // Fake database / Secret code
 const SECRET_PASSCODE = '0504';
